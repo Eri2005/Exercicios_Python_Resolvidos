64ad01cc4f3595a44eb4974528569637a617181f
Eri2005 <eri.costa05@gmail.com>
2025-02-18 23:00:21 -0300

    Exercicio

---
+++ exercicio3.7.py
@@ -0,0 +1,7 @@
+
+# Somar dois numeros Inteiros
+
+numero1 = int(input("Informe o primeiro número: "))
+numero2 = int(input("Informe o segundo número: "))
+
+print(f'A soma do número {numero1} com o número {numero2} o Resultado é =',(numero1 + numero2))
